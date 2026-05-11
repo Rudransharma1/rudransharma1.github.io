6cf431f713a7cbc032925596ddca06c0a5bbb8d8
Rudransh <157509251+Rudransharma1@users.noreply.github.com>
2026-05-11 16:51:27 -0400

    Updates

--- server/assets/_tanstack-start-manifest_v-D6X5MbX7.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/rudra/OneDrive/Desktop/Portfolio/rudransh-portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CLACgq5y.js"] }, "/": { filePath: "C:/Users/rudra/OneDrive/Desktop/Portfolio/rudransh-portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-D5J5eaMl.js"] } }, clientEntry: "/assets/index-CLACgq5y.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-DvEw7a9H.js";
-import "node:events";
-export {
-  w as default
-};
